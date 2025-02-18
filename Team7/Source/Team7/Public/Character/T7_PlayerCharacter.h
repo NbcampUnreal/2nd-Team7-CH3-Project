111295fc5bc48a5d52139105bafdaa839d3fd15d
cheeztabby <84297394+Cheeztabby@users.noreply.github.com>
2025-02-18 20:19:09 +0900

    [add] Player Character Input - Sprint, Switch Camera, Aim
    Player Character Input - Sprint, Switch Camera, Aim 추가, T7_PlayerCharacter 코드 가독성 수정. 이후 같이 사용될 애니메이션 연동해야함

--- Team7/Source/Team7/Public/Character/T7_PlayerCharacter.h
+++ Team7/Source/Team7/Public/Character/T7_PlayerCharacter.h
@@ -22,64 +22,90 @@ class TEAM7_API AT7_PlayerCharacter : public AT7_CharacterBase
 public:
 	AT7_PlayerCharacter();
 
-	//  줍기 가능한 무기
-	UPROPERTY(VisibleAnywhere, Category = "T7|Weapon")
-	AT7_Weapon* OverlappingWeapon = nullptr;
-
-
 protected:
 	void Move(const FInputActionValue& Value);
 
 	void Look(const FInputActionValue& Value);
 
 	void PickupWeapon();
+
 	void EquipWeapon(AT7_Weapon* Weapon);
+
 	void DropWeapon();
-protected:
+
+	void StartSprint();
+
+	void StopSprint();
+
+	void StartAim();
+	
+	void StopAim();
+
+	void SwitchCamera();
+
 	// 전역적인 움직임(메뉴 이동 등)은 PlayerController의 SetupInputComponent()에서
 	// 플레이어 캐릭터의 움직임은 Character의 SetupPlayerInputComponent()에서 처리하는게 일반적
 	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
-	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "T7|Input")
-	UInputMappingContext* DefaultMappingContext = nullptr;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "T7|Input")
-	UInputAction* MoveAction = nullptr;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "T7|Input")
-	UInputAction* LookAction = nullptr;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "T7|Input")
-	UInputAction* JumpAction = nullptr;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "T7|Input")
-	UInputAction* SprintAction = nullptr;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "T7|Input")
-	UInputAction* PickupAction = nullptr;
-	
-
 private:
-	UPROPERTY(VisibleAnywhere, Category="T7|Camera")
-	TObjectPtr<USpringArmComponent> SpringArmComponent = nullptr;
-
-	UPROPERTY(VisibleAnywhere, Category="T7|Camera")
-	TObjectPtr<UCameraComponent> CameraComponent = nullptr;
-
-	UPROPERTY(VisibleAnywhere, Category = "T7|Weapon")
-	AT7_Weapon* CurrentWeapon = nullptr;
-
-
 	//  무기 트리거 감지를 위한 함수
 	UFUNCTION()
-	void OnWeaponOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	void OnWeaponOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 	UFUNCTION()
-	void OnWeaponEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	void OnWeaponEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 
-	UPROPERTY(VisibleAnywhere, Category = "Combat")
-	UT7_CombatComponent* CombatComponent;
+public:
+	//  줍기 가능한 무기
+	UPROPERTY(VisibleAnywhere, Category = "T7|Weapon")
+	TObjectPtr<AT7_Weapon> OverlappingWeapon = nullptr;
 
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "T7|Input")
+	TObjectPtr<UInputMappingContext> DefaultMappingContext = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "T7|Input")
+	TObjectPtr<UInputAction> MoveAction = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "T7|Input")
+	TObjectPtr<UInputAction> LookAction = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "T7|Input")
+	TObjectPtr<UInputAction> JumpAction = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "T7|Input")
+	TObjectPtr<UInputAction> SprintAction = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "T7|Input")
+	TObjectPtr<UInputAction> PickupAction = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "T7|Input")
+	TObjectPtr<UInputAction> AimAction = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "T7|Input")
+	TObjectPtr<UInputAction> SwitchCameraAction = nullptr;
+	
+private:
+	UPROPERTY(VisibleAnywhere, Category="T7|Camera")
+	TObjectPtr<USpringArmComponent> TPSSpringArmComponent = nullptr;
+
+	UPROPERTY(VisibleAnywhere, Category="T7|Camera")
+	TObjectPtr<USpringArmComponent> FPSSpringArmComponent = nullptr;
+
+	UPROPERTY(VisibleAnywhere, Category="T7|Camera")
+	TObjectPtr<UCameraComponent> TPSCameraComponent = nullptr;
+
+	UPROPERTY(VisibleAnywhere, Category="T7|Camera")
+	TObjectPtr<UCameraComponent> FPSCameraComponent = nullptr;
+
+	UPROPERTY(VisibleAnywhere, Category = "T7|Weapon")
+	TObjectPtr<AT7_Weapon> CurrentWeapon = nullptr;
+
+	UPROPERTY(VisibleAnywhere, Category = "T7|Combat")
+	TObjectPtr<UT7_CombatComponent> CombatComponent = nullptr;
+
+	const float NormalMaxWalkSpeed = 600.0;
+
+	const float SprintMaxWalkSpeed = NormalMaxWalkSpeed * 1.5;
+
+	bool bUseTPSCamera = true;
 };
