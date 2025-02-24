07eec091fd82e62fb391c6a20a4d940c541e8d63
ArstralArsk <astralarsk@gmail.com>
2025-02-24 20:48:21 +0900

    Delete Team7/Private directory

--- Team7/Private/Character/T7_EnemyCharacter.cpp
+++
@@ -1,7 +0,0 @@
-#include "Team7/Public/Character/T7_EnemyCharacter.h"
-
-AT7_EnemyCharacter::AT7_EnemyCharacter()
-{
-	AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
-}
-
--- Team7/Private/GameMode/T7_GameModeBase.cpp
+++
@@ -1,6 +0,0 @@
-﻿#include "Team7/Public/GameMode/T7_GameModeBase.h"
-
-AT7_GameModeBase::AT7_GameModeBase()
-{
-	
-}
