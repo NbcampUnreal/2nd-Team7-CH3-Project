05279b7480f047ea2c080f677529c440c15e53aa
cheeztabby <84297394+Cheeztabby@users.noreply.github.com>
2025-02-25 16:44:35 +0900

    [add] T7_AssetManager

---
+++ Team7/Source/Team7/Public/System/T7_AssetManager.h
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Engine/AssetManager.h"
+#include "T7_AssetManager.generated.h"
+
+class UT7_DataAssets;
+/**
+ * 
+ */
+UCLASS()
+class TEAM7_API UT7_AssetManager : public UAssetManager
+{
+	GENERATED_BODY()
+public:
+	UT7_AssetManager();
+
+	static UT7_AssetManager& Get();
+
+	static UObject* GetAssetByTag(const FGameplayTag& Tag);
+
+	static void LoadAssetSynchronouslyByTag(const FGameplayTag& Tag);
+
+	void InitializeDataAssets();
+
+private:
+	virtual void StartInitialLoading() override;
+	
+	void AddLoadedAsset(UObject* Asset);
+	
+	UPROPERTY()
+	TObjectPtr<UT7_DataAssets> LoadedDataAssets = nullptr;
+
+	// GC 수집 방지
+	UPROPERTY()
+	TArray<TObjectPtr<UObject>> LoadedAssets;
+};
