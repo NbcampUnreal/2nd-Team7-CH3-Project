c00177a49bf66ff950474dc02172758d1df5c96a
cheeztabby <84297394+Cheeztabby@users.noreply.github.com>
2025-02-25 16:44:47 +0900

    [add] T7_GameInstance

---
+++ Team7/Source/Team7/Private/System/T7_GameInstance.cpp
@@ -0,0 +1,15 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "System/T7_GameInstance.h"
+#include "System/T7_AssetManager.h"
+
+UT7_GameInstance::UT7_GameInstance()
+{
+}
+
+void UT7_GameInstance::Init()
+{
+	Super::Init();
+	UT7_AssetManager::Get().InitializeDataAssets();
+}
---
+++ Team7/Source/Team7/Public/System/T7_GameInstance.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "T7_GameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEAM7_API UT7_GameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+public:
+	UT7_GameInstance();
+
+private:
+	virtual void Init() override;
+	
+};
