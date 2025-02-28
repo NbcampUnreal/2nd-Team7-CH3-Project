16b1518808ea2c4ad643ec2d8ac7f9847e919438
cheeztabby <84297394+Cheeztabby@users.noreply.github.com>
2025-02-28 14:03:33 +0900

    [Add] T7_SpawnData

---
+++ Team7/Source/Team7/Private/Data/T7_SpawnData.cpp
@@ -0,0 +1,14 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Data/T7_SpawnData.h"
+
+UClass* UT7_SpawnData::FindClassByTag(const FGameplayTag& Tag)
+{
+	if (SpawnDataMap[Tag])
+	{
+		return SpawnDataMap[Tag].Get();	
+	}
+	UE_LOG(LogTemp,Error,TEXT("Can't find BP Class for ClassTag [%s]"), *Tag.ToString());
+	return nullptr;
+}
---
+++ Team7/Source/Team7/Public/Data/T7_SpawnData.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Engine/DataAsset.h"
+#include "T7_SpawnData.generated.h"
+
+UCLASS()
+class TEAM7_API UT7_SpawnData : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UClass* FindClassByTag(const FGameplayTag& Tag);
+	
+	UPROPERTY(EditDefaultsOnly)
+	TMap<FGameplayTag, TSubclassOf<AActor>> SpawnDataMap;
+};
