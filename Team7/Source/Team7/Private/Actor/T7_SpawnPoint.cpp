502fbd6c2e0d7bfea4e916f538dc32de4d016d65
cheeztabby <84297394+Cheeztabby@users.noreply.github.com>
2025-02-28 14:03:18 +0900

    [Add] T7_SpawnPoint

---
+++ Team7/Source/Team7/Private/Actor/T7_SpawnPoint.cpp
@@ -0,0 +1,27 @@
+﻿#include "Actor/T7_SpawnPoint.h"
+
+#include "GameMode/T7_GameModeBase.h"
+#include "System/T7_GameStateBase.h"
+
+AT7_SpawnPoint::AT7_SpawnPoint()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AT7_SpawnPoint::BeginPlay()
+{
+	Super::BeginPlay();
+	Register();
+}
+
+void AT7_SpawnPoint::Register()
+{
+	if (UWorld* World = GetWorld())
+	{
+		if (AT7_GameStateBase* GameState = World->GetGameState<AT7_GameStateBase>())
+		{
+			GameState->RegisterSpawnPoint(this);
+		}
+	}
+}
+
